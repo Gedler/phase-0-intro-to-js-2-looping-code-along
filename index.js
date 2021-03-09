53393b5ae468d4ece14c3afd1f19b96aa4fdb7d5
Gedler <gedler.jeanpierre@stonybrook.edu>
2021-03-09 23:01:28 +0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+let nameList = [];
+function writeCards(name, party) {
+    for (let x= 0; x<name.length; x++) {
+        nameList.push(`Thank you, ${name[x]}, for the wonderful ${party} gift!`)
+               
+    } 
+    return nameList;
+} 
+function countDown(number) {
+    for (let z = number; z>=0; z-- ) { 
+        console.log(z)
+
+    }
+}
+
+
+
+
+
+writeCards();
